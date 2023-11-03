275ce33aa797e18d39ec3e310f5da41abb1ae8c6
Imre Kli. <imre.koli@tktk.ee>
2023-11-03 15:57:10 +0200

    add week1 files

---
+++ MyFirstApp/Program.cs
@@ -0,0 +1,14 @@
+﻿// This is a comment to my code
+
+
+//rakendus küsib kasutajalt sisestada tema nime
+//rakendus tervitab kasutajat nimepidi
+
+Console.WriteLine("Enter your name");
+//string - sõne
+string userName = Console.ReadLine();
+
+Console.WriteLine("Hello" + ", "  + userName + "!");
+//string interpolation
+
+Console.WriteLine($"Hello, {userName}!");
